f365658161f3f9fb0d546366366ea4ceca834fbc
George Matos <georgemattos@gmail.com>
2020-01-21 21:32:07 -0300

    First commit - Project Created

---
+++ src/main/java/com/cursoudemy/springboot2mongodb/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cursoudemy.springboot2mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
