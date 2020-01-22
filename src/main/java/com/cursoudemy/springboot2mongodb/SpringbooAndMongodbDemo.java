f77982e85f72a76b999f0b1046e2047f49bdf80c
George Matos <georgemattos@gmail.com>
2020-01-22 17:50:18 -0300

    App name update

--- src/main/java/com/cursoudemy/springboot2mongodb/SpringbooAndMongodbDemo.java
+++ src/main/java/com/cursoudemy/springboot2mongodb/SpringbooAndMongodbDemo.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringbooAndMongodbDemo {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringbooAndMongodbDemo.class, args);
 	}
 
 }
