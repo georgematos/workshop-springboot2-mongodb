e605ef9a06581402133254969a64efe8a585a8bf
George Matos <georgemattos@gmail.com>
2020-01-27 18:32:26 -0300

    Inserting comments to posts

--- src/main/java/com/cursoudemy/springboot2mongodb/config/Instantiation.java
+++ src/main/java/com/cursoudemy/springboot2mongodb/config/Instantiation.java
@@ -7,6 +7,7 @@ import java.util.TimeZone;
 import com.cursoudemy.springboot2mongodb.domain.Post;
 import com.cursoudemy.springboot2mongodb.domain.User;
 import com.cursoudemy.springboot2mongodb.dto.AuthorDTO;
+import com.cursoudemy.springboot2mongodb.dto.CommentDTO;
 import com.cursoudemy.springboot2mongodb.repository.PostRepository;
 import com.cursoudemy.springboot2mongodb.repository.UserRepository;
 
@@ -47,6 +48,13 @@ public class Instantiation implements CommandLineRunner {
     Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!",
         new AuthorDTO(maria));
     Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+    CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+    CommentDTO comment2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+    CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+    post1.getComments().addAll(Arrays.asList(comment1, comment2));
+    post2.getComments().add(comment3);
 
     postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/cursoudemy/springboot2mongodb/domain/Post.java
+++ src/main/java/com/cursoudemy/springboot2mongodb/domain/Post.java
@@ -1,9 +1,12 @@
 package com.cursoudemy.springboot2mongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.cursoudemy.springboot2mongodb.dto.AuthorDTO;
+import com.cursoudemy.springboot2mongodb.dto.CommentDTO;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -22,6 +25,8 @@ public class Post implements Serializable {
   private String title;
   private String body;
   private AuthorDTO author;
+
+  private List<CommentDTO> comments = new ArrayList<>();
 
   public Post() {
   }
@@ -72,6 +77,14 @@ public class Post implements Serializable {
 
   public void setAuthor(AuthorDTO author) {
     this.author = author;
+  }
+
+  public List<CommentDTO> getComments() {
+    return comments;
+  }
+
+  public void setComments(List<CommentDTO> comments) {
+    this.comments = comments;
   }
 
   @Override
