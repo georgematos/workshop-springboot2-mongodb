219a1f869e6bc85bfe2cfa96699e60c330b6a55c
George Matos <georgemattos@gmail.com>
2020-01-27 14:42:03 -0300

    End point to return a user posts

--- src/main/java/com/cursoudemy/springboot2mongodb/resources/UserResource.java
+++ src/main/java/com/cursoudemy/springboot2mongodb/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.cursoudemy.springboot2mongodb.domain.Post;
 import com.cursoudemy.springboot2mongodb.domain.User;
 import com.cursoudemy.springboot2mongodb.dto.UserDTO;
 import com.cursoudemy.springboot2mongodb.service.UserService;
@@ -14,11 +15,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -51,6 +52,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         UserDTO userDto = new UserDTO(service.findById(id));
         return ResponseEntity.ok().body(userDto);
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User user = service.findById(id);
+        List<Post> posts = user.getPosts();
+        return ResponseEntity.ok().body(posts);
     }
 
     @PostMapping
